4f6c9b4d2514f325a5032b8e4bfad7398ad2abff
zmccoll <zachmccoll@gmail.com>
2024-02-05 20:04:37 -0800

    update index.jsx
    moved back to src folder, updated app to render main-view page

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import { createRoot } from 'react-dom/client';
+import { MainView } from './components/main-view/main-view'
+
+// Import statement to indcate that you need to bundle `./index.scss`
+import './index.scss';
+
+// Main component (we will eventually use all the others)
+const myFlixApplication = () => {
+    return <MainView />;
+};
+
+// Finds the root of your app
+const container = document.querySelector('#root');
+const root = createRoot(container);
+
+// Tells React to render your app in the root DOM element
+root.render(<myFlixApplication />);
