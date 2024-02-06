89cc5fdc052c3cc4b7d10705ad1b8d3516091dff
zmccoll <zachmccoll@gmail.com>
2024-02-05 20:11:46 -0800

    update index.jsx
    added capitalization

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import { createRoot } from 'react-dom/client';
-import { MainView } from './components/main-view/main-view'
+import { MainView } from './components/main-view/main-view'; //importing MainView component
 
 // Import statement to indcate that you need to bundle `./index.scss`
 import './index.scss';
 
 // Main component (we will eventually use all the others)
-const myFlixApplication = () => {
+const MyFlixApplication = () => {
     return <MainView />;
 };
 
@@ -14,4 +14,4 @@ const container = document.querySelector('#root');
 const root = createRoot(container);
 
 // Tells React to render your app in the root DOM element
-root.render(<myFlixApplication />);
+root.render(<MyFlixApplication />);
