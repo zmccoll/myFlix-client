29d5b2d63046f2e4f784c208b74ee7f4c50aedc7
zmccoll <zachmccoll@gmail.com>
2024-02-05 19:39:30 -0800

    update movie-view.jsx
    adjusted spacing

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,17 +1,17 @@
 export const MovieView = ({ movie }) => { //this deals with how the movies will be displayed when they get clicked on
     return (
         <div>
-                    <div>
-            <img src={movie.image} />
-        </div>
-        <div>
-            <span>Title</span>
-            <span>{movie.title}</span>
-        </div>
-        <div>
-            <span>Director</span>
-            <span>{movie.director}</span>
-        </div>
+            <div>
+                <img src={movie.image} />
+            </div>
+            <div>
+                <span>Title</span>
+                <span>{movie.title}</span>
+            </div>
+            <div>
+                <span>Director</span>
+                <span>{movie.director}</span>
+            </div>
         </div>
 
     );
