3e7cd6e5bf4beb7ee593f28b7dae4c012b9e9242
zmccoll <zachmccoll@gmail.com>
2024-02-10 23:40:36 -0800

    update movie-view.jsx
    added more data to movie view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -5,12 +5,20 @@ export const MovieView = ({ movie, onBackClick }) => { //this deals with how the
                 <img src={movie.image} />
             </div>
             <div>
-                <span>Title</span>
+                <span>Title:</span>
                 <span>{movie.title}</span>
             </div>
             <div>
-                <span>Director</span>
-                <span>{movie.director}</span>
+                <span>Description:</span>
+                <span>{movie.Description}</span>
+            </div>
+            <div>
+                <span>Genre:</span>
+                <span>{movie.Genre.name}</span>
+            </div>
+            <div>
+                <span>Director:</span>
+                <span>{movie.Director.name}</span>
             </div>
             <div>
                 <button onClick={onBackClick}>Back</button>
